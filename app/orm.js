0e6ae33eaf8b076bd0bb27e1fb5e8dd408808138
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-06-26 16:49:48 -0400

    Figured out the html page with css

--- app/orm.js
+++ app/orm.js
@@ -1,19 +1,56 @@
-const db = require( './connection' )
+const mysql = require("mysql");
 
-function selectAll(){
-    return db.query( "SELECT * FROM burgers" )
+class Database {
+  constructor( config ) {
+      this.connection = mysql.createConnection( config );
+  }
+  query( sql, args ) {
+      return new Promise( ( resolve, reject ) => {
+          this.connection.query( sql, args, ( err, rows ) => {
+              if ( err )
+                  return reject( err );
+              resolve( rows );
+          } );
+      } );
+  }
+  close() {
+      return new Promise( ( resolve, reject ) => {
+          this.connection.end( err => {
+              if ( err )
+                  return reject( err );
+              resolve();
+          } );
+      } );
+  }
 }
 
-function insertOne( name ){
-    return db.query( "INSERT INTO burgers SET ? ", 
-        { burger_name: name, devoured: false } )
+const db = new Database({
+  host: "localhost",
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  insecureAuth : true
+});
+
+function selectList( criteria={} ){
+    return db.query( "SELECT * FROM list "+( criteria ? "WHERE ? " : "" ), criteria )
 }
 
-function updateOne( id, field, value ){
-    return db.query( "UPDATE burgers SET ? WHERE id=?", 
+function insertItem( name ){
+    return db.query( "INSERT INTO list SET ? ", 
+        { name, complete: false } )
+}
+
+function updateItem( id, field, value ){
+    return db.query( "UPDATE list SET ? WHERE id=?", 
         [ { [field]: value}, id ] )
 }
 
+function deleteItem( id ){
+    return db.query( "DELETE FROM list WHERE id=?", [ id ] )
+}
+
 module.exports = {
-    selectAll, insertOne, updateOne
+    insertItem, insertItem, updateItem, deleteItem
 }
